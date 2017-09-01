16c05aff9aefcf350473e276cdf8ed1f45e614aa
rdjah <lisandro.percoco@hotmail.com>
2017-09-01 19:49:37 -0300

    Interfaz home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer.component';
+import { HeaderComponent } from './header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
