21d05a6b0589430fb6341321e98280022b3e28b7
rdjah <lisandro.percoco@hotmail.com>
2017-09-06 15:51:05 -0300

    creacion del componente caja de busqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer.component';
 import { HeaderComponent } from './header.component';
-
+import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-movie-search',
+  templateUrl: './search.component.html'
+})
+
+export class SearchComponent {
+
+  searchString : String;
+
+  onClickMe() {
+    //codigo busqueda
+  }
+
+}
