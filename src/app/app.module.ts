b6558877efa137af861a6744010c04b0fa28f166
rdjah <lisandro.percoco@hotmail.com>
2017-09-06 17:35:14 -0300

    organizacion de directorios del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer.component';
-import { HeaderComponent } from './header.component';
-import { SearchComponent } from './search.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
